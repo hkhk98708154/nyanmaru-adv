e183559caa90a22b680004cd48ac5b7d4d560c31
hkhk98708154 <hkhk01312022@gmail.com>
2025-05-08 00:18:35 +0900

    fix: パス修正

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
-import Image from "next/image";
-import NyanmaruADV from './components/NyanmaruADV';
+// app/page.tsx
+import NyanmaruADV from "@/app/components/NyanmaruADV";
 
 export default function Home() {
   return <NyanmaruADV />;
 }
+
 
 
